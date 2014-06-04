52fc5d87e11994db011c2ded6beefaae8e0ded5d
wdong@wdong.org <wdong@wdong.org@d16f79c6-2e22-58ed-2b30-9f588bc3f9b6>
2014-06-04 17:50:17 +0000

    Fixed example.sh.

--- script/example.sh
+++ script/example.sh
@@ -2,9 +2,11 @@
 
 # SET THE FOLLOWING THREE PARAMETERS BEFORE YOU RUN
 
-IMAGE_DIR="$HOME/src/bang/c++/data/[0-9]*"
+IMAGE_DIR="$HOME/src/ida/neardupimages1"
 HADOOP_WORK_DIR=nise-tmp
 OUTPUT_DIR=.
+
+PARTITIONS=10
 
 if [ -z "$IMAGE_DIR" -o -z "$HADOOP_WORK_DIR" ]
 then
@@ -18,43 +20,35 @@ then
     exit
 fi
 
+#TOTAL=4250
 
 if false
 then
 hadoop -mkdir $HADOOP_WORK_DIR
-
 find $IMAGE_DIR -type f | awk '{print $1, $1;}' | $NISE_HOME/bin/import --output $HADOOP_WORK_DIR/import
-
-TOTAL=`$NISE_HOME/bin/number $HADOOP_WORK_DIR/import $HADOOP_WORK_DIR/number`
+TOTAL=`$NISE_HOME/bin/number --partitions $PARTITIONS $HADOOP_WORK_DIR/import $HADOOP_WORK_DIR/number`
+echo $TOTAL
 
 if [ -z "$TOTAL" ]; then exit; fi
 
-$NISE_HOME/bin/group --total $TOTAL $HADOOP_WORK_DIR/number $HADOOP_WORK_DIR/group
-hadoop fs -getmerge $HADOOP_WORK_DIR/group images
+$NISE_HOME/bin/group --conf mapred.reduce.tasks=$PARTITIONS --total $TOTAL $HADOOP_WORK_DIR/number $HADOOP_WORK_DIR/group
+$NISE_HOME/bin/graph-hash --conf mapred.reduce.tasks=$PARTITIONS $HADOOP_WORK_DIR/number $HADOOP_WORK_DIR/graph-hash
 
+
+$NISE_HOME/bin/graph-join --conf mapred.reduce.tasks=$PARTITIONS $HADOOP_WORK_DIR/graph-hash $HADOOP_WORK_DIR/graph-join
+
+$NISE_HOME/bin/download $HADOOP_WORK_DIR/group images
 $NISE_HOME/bin/group-index < images > images.idx
-
-
-$NISE_HOME/bin/graph-hash $HADOOP_WORK_DIR/number $HADOOP_WORK_DIR/graph-hash
-
-
-$NISE_HOME/bin/graph-join $HADOOP_WORK_DIR/graph-hash $HADOOP_WORK_DIR/graph-join
-
-hadoop fs -getmerge $HADOOP_WORK_DIR/graph-join graph
-
-fi
+$NISE_HOME/bin/download $HADOOP_WORK_DIR/graph-join graph
 
 for off in 0 32 64 96
 do
 
-$NISE_HOME/bin/sketch-index --offset $off $HADOOP_WORK_DIR/number $HADOOP_WORK_DIR/sketch.$off
-hadoop fs -getmerge $HADOOP_WORK_DIR/sketch.$off sketch.$off
+$NISE_HOME/bin/sketch-index --conf mapred.reduce.tasks=$PARTITIONS --offset $off $HADOOP_WORK_DIR/number $HADOOP_WORK_DIR/sketch.$off
+$NISE_HOME/bin/download $HADOOP_WORK_DIR/sketch.$off sketch.$off
 fbi-trie -I sketch.$off -O sketch.$off.trie -F $off
 
 done
-
-exit
-
 
 DIR=`pwd`
 
@@ -63,10 +57,10 @@ cat > db <<FOO
 4
 1000
 $DIR
-0 sketch.0 sketch.0.trie
-32 sketch.32 sketch.32.trie
-64 sketch.64 sketch.64.trie
-96 sketch.96 sketch.96.trie
+0 0 sketch.0 sketch.0.trie
+32 0 sketch.32 sketch.32.trie
+64 0 sketch.64 sketch.64.trie
+96 0 sketch.96 sketch.96.trie
 FOO
 
 cat > server.xml <<FOO
@@ -100,3 +94,4 @@ cat > server.xml <<FOO
 FOO
 
 echo If no error occurred, databases are built.  You can start server at this directory.
+
